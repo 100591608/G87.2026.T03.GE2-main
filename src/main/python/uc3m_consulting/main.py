2c2cd4dccc2738883b345bb3d63f60191777c280
pablo-velarde <100591608@alumnos.uc3m.es>
2026-03-02 20:42:23 +0100

    Add freezegun to requirements.txt and add test cases TC5 to TC33.

--- src/main/python/uc3m_consulting/main.py
+++ src/main/python/uc3m_consulting/main.py
@@ -5,10 +5,10 @@ from uc3m_consulting import enterprise_project
 def showMD5():
     company_cif = "A58818501"
     project_acronym = "PROJECT12"
-    project_description = "competition development test"
+    project_description = "competition development tests"
     department = "LEGAL"
     date = "30/11/2027"
-    budget = 1000000
+    budget = 1000000.00
 
     obj= enterprise_project.EnterpriseProject(company_cif,project_acronym,project_description,department, date,budget)
     print(obj.project_id)
--- src/unittest/python/test_enterprise_manager.py
+++ src/unittest/python/test_enterprise_manager.py
@@ -1,6 +1,7 @@
 """Testing file for EnterpriseManager"""
 from unittest import TestCase
 from uc3m_consulting import EnterpriseManager
+from uc3m_consulting import EnterpriseManagementException
 
 
 class TestEnterpriseManager(TestCase):
@@ -33,13 +34,13 @@ class TestEnterpriseManager(TestCase):
         enterprise_manager = EnterpriseManager()
         company_cif = "A58818501"
         project_acronym = "PROJECT12"
-        project_description = "competition development test"
+        project_description = "competition development tests"
         department = "LEGAL"
         date = "30/11/2027"
-        budget = 1000000
+        budget = 1000000.00
         output = enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
                                                      date, budget)
-        self.assertEqual("96749da7fd992e68b7c87fa8831a7001", output)
+        self.assertEqual("6241127df42cb7486858e3c8b46b58c2", output)
 
     def test_TC4(self):
         enterprise_manager = EnterpriseManager()
@@ -54,88 +55,378 @@ class TestEnterpriseManager(TestCase):
         self.assertEqual("b21123066aa51951c4887d33c68faf0d", output)
 
     def test_TC5(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = 123
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Company CIF")
 
     def test_TC6(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818502"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                         date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Company CIF")
 
     def test_TC7(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A588185012"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                         date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Company CIF")
 
     def test_TC8(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A5881850"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                         date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Company CIF")
 
     def test_TC9(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "158818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                         date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Company CIF")
 
     def test_TC10(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = 10000
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Acronym")
 
     def test_TC11(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Acronym")
 
     def test_TC12(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJECT1234"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Acronym")
 
     def test_TC13(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "proj1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Acronym")
 
     def test_TC14(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = 1234567890
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Description")
 
     def test_TC15(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "blueberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Description")
 
     def test_TC16(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "competitions developments tests"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Project Description")
 
     def test_TC17(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = 123
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Department")
 
     def test_TC18(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "SALES"
+        date = "01/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Department")
 
     def test_TC19(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = 1/1/2025 # Should maybe just change to int
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC20(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/31/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC21(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "00/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC22(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "32/01/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC23(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/00/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC24(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/13/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC25(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2024"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC26(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2028"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC27(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/02/2025"
+        budget = 50000.00
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Date")
 
     def test_TC28(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = "50000.00"
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Budget")
 
     def test_TC29(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.0
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Budget")
 
     def test_TC30(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.000
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Budget")
 
     def test_TC31(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 49999.99
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Budget")
 
     def test_TC32(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 1000000.01
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Invalid Budget")
 
     def test_TC33(self):
-        self.fail()
+        enterprise_manager = EnterpriseManager()
+        company_cif = "A58818501"
+        project_acronym = "PROJ1"
+        project_description = "strawberry"
+        department = "HR"
+        date = "01/01/2025"
+        budget = 50000.01
+        with self.assertRaises(EnterpriseManagementException) as cm:
+            enterprise_manager.register_project(company_cif, project_acronym, project_description, department,
+                                                date, budget)
+        self.assertEqual(cm.exception.message, "Project with same name for the same CIF already existed in the data file")
